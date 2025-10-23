8cca9ff3dea59266cd311ecc024a01983f1deb09
Noah <noahdiakowski@gmail.com>
2025-10-23 10:13:09 +0200

    chore: change design to match resolution
    change the layout to match the resolution of the showing screen

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const deadline = new Date("2025-10-30T00:00:00Z");
 
 function getTimeRemaining(endtime){
     const total = endtime - Date.now(); 
-    const seconds = Math.floor((total / 1000) % 60);
     const minutes = Math.floor((total / 1000 / 60) % 60);
     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
     const days = Math.floor(total / (1000 * 60 * 60 * 24));
@@ -11,8 +10,7 @@ function getTimeRemaining(endtime){
         total,
         days,
         hours,
-        minutes,
-        seconds
+        minutes
     };
 }
 
@@ -28,5 +26,5 @@ function updateCount(){
     + total.hours + " Timer | " + total.minutes + " Minutter";
 }
 
-const timer = setInterval(updateCount, 10000000);
+const timer = setInterval(updateCount, 60000);
 updateCount();
