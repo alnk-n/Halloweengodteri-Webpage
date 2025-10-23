12dd06d95f27e798aa60a505aca233f816210d44
Noah <noahdiakowski@gmail.com>
2025-10-23 10:23:26 +0200

    prefix: updating js script

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 const deadline = new Date("2025-10-30T00:00:00Z");
 
-function getTimeRemaining(endtime){
-    const total = endtime - Date.now(); 
-    const minutes = Math.floor((total / 1000 / 60) % 60);
-    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-    const days = Math.floor(total / (1000 * 60 * 60 * 24));
-    
-    return{
-        total,
-        days,
-        hours,
-        minutes
-    };
+function getTimeRemaining(endtime) {
+  const total = endtime - Date.now();
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return { total, days, hours, minutes };
 }
 
-function updateCount(){
-    while (true){
-        const countdownTime = document.getElementById("changeJS");
-        const total = getTimeRemaining(deadline);
+function updateCount() {
+  const countdownTime = document.getElementById("changeJS");
+  const total = getTimeRemaining(deadline);
 
-        if (total.total <= 0) {
-        clearInterval(timer);
-        return;
-    }
-        countdownTime.innerHTML = total.days + " Dager | "
-        + total.hours + " Timer | " + total.minutes + " Minutter";
-    }
+  if (total.total <= 0) {
+    clearInterval(timer);
+    countdownTime.innerHTML = "Tiden er ute!";
+    return;
+  }
+
+  countdownTime.innerHTML =
+    total.days + " Dager | " +
+    total.hours + " Timer | " +
+    total.minutes + " Minutter";
 }
 
 const timer = setInterval(updateCount, 60000);
